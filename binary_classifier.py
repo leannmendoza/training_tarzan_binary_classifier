8ef10691c1a2aec017b136e4a8164ac1914698af
LeAnn Mendoza <44107227+leannmendoza@users.noreply.github.com>
2022-04-21 20:55:42 -0700

    Delete organize_files fx, add clargs, implement overwrite

--- binary_classifier.py
+++ binary_classifier.py
@@ -19,59 +19,45 @@ import shutil
 import pandas as pd
 import preprocess_data
 from preprocess_data import add_none_column, df_to_csv, show_image_with_label
+import argparse
 
-def organize_files(filenames):
-	"""
-	params: filenames
-	return: dirs
-	Takes list of filenames and for each file within the filename organizes the files
-	by creating directories. i.e. train/dislikes and train/dislikes. This makes it easier
-	to simply pass in a directory path into the train_model function.
-	"""
-	dirs = []
-	for file in filenames:
-		dataset_type = file.split('.')[0]
-		new_dataset_dir = os.getcwd() + '/' + dataset_type
-		dirs.append(new_dataset_dir)
-		if not os.path.exists(new_dataset_dir):
-			os.makedirs(new_dataset_dir)
-			os.makedirs(new_dataset_dir + '/likes/')
-			os.makedirs(new_dataset_dir + '/dislikes/')
-			print("The new directory", new_dataset_dir,"is created! Adding Images...")
 
-		with open(file) as f:
-			next(f)
-			for line in f:
-				img_path, truth = line.strip().split(',')
-				img_name = img_path.split('/')[-1]
-				new_dataset_dir_truth = new_dataset_dir + '/' + truth + '/'
-				new_file_path = new_dataset_dir_truth + img_name
-				if not os.path.exists(new_file_path): 
-					shutil.copy(os.path.abspath(img_path), new_dataset_dir_truth)
-			print('All images added to', dataset_type, 'directory!')
-	return dirs
-
-def train_model(path_to_train, path_to_validation):
+def train_model(path_to_train_csv, path_to_validation_csv, model_filename, overwrite):
 	"""
 	params: path_to_train, path_to_validation
 	return: model
 	This function constructs the ml model
 	"""
-	if not exists('training_tarzan.h5'):
+	if os.path.exists(model_filename):
+		if not overwrite:
+			print("Retrieving previously trained model...")
+			model = keras.models.load_model(model_filename)
+			return model
+
+	if os.path.exists(path_to_train_csv) and os.path.exists(path_to_validation_csv):
 		print("*********************************\n"\
 			  "       Training Model            \n"\
 			  "*********************************")
+		train_df = pd.read_csv(path_to_train_csv)
+		validation_df = pd.read_csv(path_to_validation_csv)
+
 		train = ImageDataGenerator(rescale=1/255)
 		validation = ImageDataGenerator(rescale=1/255)
 
-		train_dataset = train.flow_from_directory(directory = path_to_train, 
+		train_dataset = train.flow_from_dataframe(dataframe=train_df,
 												target_size=(150,150),
+												x_col="img_path",
+												y_col="truth",
 												batch_size = 32,
+												validate_filenames=True,
 												class_mode = 'binary')
 											 
-		validation_dataset = validation.flow_from_directory(directory = path_to_validation, 
+		validation_dataset = validation.flow_from_dataframe(dataframe=validation_df,
 												target_size=(150,150),
+												x_col="img_path",
+												y_col="truth",
 												batch_size =32,
+												validate_filenames=True,
 												class_mode = 'binary')
 		print(train_dataset.class_indices)
 		print(validation_dataset.class_indices)
@@ -115,36 +101,31 @@ def train_model(path_to_train, path_to_validation):
 				validation_data = validation_dataset
 			)
 
-		model.save("training_tarzan.h5")
+		model.save(model_filename)
 	else:
-		print("*********************************\n"\
-		  	  "    Loading Trained Model        \n"\
-		      "*********************************")
-		model = keras.models.load_model('training_tarzan.h5')
+		raise FileNotFoundError('Training and/or Validation file does not exist.')
 	return model
 
-def predict_image(model, filename):
+def predict_image(model, filename, class1_label, class2_label):
 	"""
 	params: model, filename
 	return: val
 	See how the model performs on one image.
 	"""
-	print('Testing model performance on', filename, '...')
 	img1 = image.load_img(filename,target_size=(150,150))
 	plt.imshow(img1)
 	Y = image.img_to_array(img1)
 	X = np.expand_dims(Y,axis=0)
 	val = model.predict(X)[0]
-	print(val)
 	if val >= 0.5:
-		plt.xlabel("likes",fontsize=10)
+		plt.xlabel(class1_label,fontsize=10)
 	else:
-		plt.xlabel("dislikes",fontsize=10)
+		plt.xlabel(class2_label,fontsize=10)
 	plt.show()
 
 	return val
 
-def predict_test(model, test_filename, save_file=None):
+def predict_test(model, class1_label, class2_label, test_filename, predictions_filename, overwrite):
 	"""
 	params: model, test_filename, save_file=None
 	return: df
@@ -153,36 +134,65 @@ def predict_test(model, test_filename, save_file=None):
 	print("*********************************\n"\
 		  "    Calculating Predictions      \n"\
 		  "*********************************")
-	if not os.path.exists("predictions.csv"):
-		print('Testing model performance on independant test set...')
-		df = pd.read_csv(test_filename)
-		add_none_column(df)
-		for index, row in tqdm(df.iterrows()):
-			img1 = image.load_img(row['img_path'],target_size=(150,150))
-			Y = image.img_to_array(img1)
-			X = np.expand_dims(Y,axis=0)
-			val = model.predict(X)[0]
-			# round our results (although mostly 0/1)
-			if val >= 0.5:
-				prediction = 'likes'
-			else:
-				prediction = 'dislikes'
-			df.iloc[index, df.columns.get_loc('prediction')] = prediction
-		print(df)
-		if save_file:
-			df_to_csv(df, 'predictions.csv')
-			print('Predictions saved!')
-	else:
-		df = pd.read_csv("predictions.csv")
+	if os.path.exists("predictions.csv"):
+		if not overwrite:
+			print("Retrieving previously saved predictions...")
+			df = pd.read_csv(predictions_filename)
+			return df
+	print('Testing model performance on independant test set...')
+	df = pd.read_csv(test_filename)
+	add_none_column(df)
+	for index, row in tqdm(df.iterrows()):
+		img1 = image.load_img(row['img_path'],target_size=(150,150))
+		Y = image.img_to_array(img1)
+		X = np.expand_dims(Y,axis=0)
+		val = model.predict(X)[0]
+		# round our results (although mostly 0/1)
+		if val >= 0.5:
+			prediction = class1_label
+		else:
+			prediction = class2_label
+		df.iloc[index, df.columns.get_loc('prediction')] = prediction
+	print(df)
+		
+	df_to_csv(df, predictions_filename, overwrite)
+	print('Predictions saved!')
 	return df
 
 
 def main():
-	csvs = ['train.csv', 'validation.csv', 'test.csv']
-	dirs = organize_files(csvs)
-	model = train_model(dirs[0]+'/', dirs[1]+'/')
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('class1_label', type=str, help='Label for images of class1')
+	parser.add_argument('class2_label', type=str, help='Label for images of class1')
+	parser.add_argument('-tr', '--train_filename', type=str, help='Filename to save training csv')
+	parser.add_argument('-v', '--validation_filename', type=str, help='Filename to save validation csv')
+	parser.add_argument('-te', '--test_filename', type=str, help='Filename to save test csv')
+	parser.add_argument('-p', '--predictions_filename', type=str, help='Filename to save predictions csv')
+	parser.add_argument('-m', '--model_filename', type=str, help='Filename to save model (.h5 file)')
+	parser.add_argument('-o', '--overwrite', action='store_true', help='Overwrite existing files and model (used for constructing new model)')
+	parser.set_defaults(train_filename="train.csv")
+	parser.set_defaults(validation_filename="validation.csv")
+	parser.set_defaults(test_filename="test.csv")
+	parser.set_defaults(predictions_filename="predictions.csv")
+	parser.set_defaults(model_filename="model.h5")
+
+	args = parser.parse_args()
+	c1_label = args.class1_label
+	c2_label = args.class2_label
+	train_file = args.train_filename
+	validation_file = args.validation_filename
+	test_file = args.test_filename
+	predictions_file = args.predictions_filename
+	model_file = args.model_filename
+	overwrite = args.overwrite
+
+	model = train_model(train_file, validation_file, model_file, overwrite)
 	model.summary()
-	#predict_test(model, 'test.csv', save_file = True)
+	predict_test(model, c1_label, c2_label, test_file, predictions_file, overwrite)
+	df = pd.read_csv(test_file)
+	for i in range(10):
+		predict_image(model, df['img_path'][i], c1_label, c2_label)
 	
 if __name__ == '__main__':
 	main()
