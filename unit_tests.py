a6793be500b434450aa79f5aa83872965715d9f3
LeAnn Mendoza <44107227+leannmendoza@users.noreply.github.com>
2022-04-22 12:00:49 -0700

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,35 +1,40 @@
 import unittest
 import pandas as pd
+import ctypes
+import os
 from preprocess_data import *
 from analysis import *
 from binary_classifier import *
 
-truth_df = pd.DataFrame()
 class TestPreprocessMethods(unittest.TestCase):
 
-	def test_create_binary_df(self):
-		likes_path = "./data_for_unit_tests/likes/"
-		dislikes_path = "./data_for_unit_tests/dislikes/"
-		truth_df = create_binary_df(likes_path,dislikes_path)
-		self.assertIsInstance(truth_df, pd.core.frame.DataFrame)
-		self.assertEqual(truth_df.size, 128)
+	def test_print_binary_truth_counts1(self):
+		df = pd.DataFrame({'img_path':['a.jpg','b.jpg','c.jpg'],'truth':[0,1,1]})
+		self.assertEqual(print_binary_truth_counts(df, "test"), (1,2))
 
-	def test_split_data(self):
-		train_df, test_df, validation_df = split_data(truth_df)
-		self.assertIsInstance(split_data(truth_df), tuple)
-		df_to_csv(train_df, "./data_for_unit_tests/train.csv")
-		df_to_csv(validation_df, "./data_for_unit_tests/validation.csv")
-		df_to_csv(test_df, "./data_for_unit_tests/test.csv")
+	def test_print_binary_truth_counts2(self):
+		df = pd.DataFrame({'img_path':['a.jpg','b.jpg','c.jpg'],'truth':[0,0,1]})
+		self.assertEqual(print_binary_truth_counts(df, "test"), (2,1))
 
 	def test_add_none_column(self):
 		df = pd.DataFrame([1,2,3])
 		self.assertEqual(add_none_column(df).size, 6)
 
-class TestBinaryClassifier(unittest.TestCase):
+	def test_df_to_csv_file_creation(self):
+		df = pd.DataFrame({'img_path':['a.jpg','b.jpg','c.jpg'],'truth':[0,1,1]})
+		csv_filename = 'temp_unit_testing.csv'
+		overwrite = False
+		df_to_csv(df, csv_filename, overwrite)
+		self.assertTrue(os.path.exists(csv_filename))
 
-	def test_organize_files(self):
-		csvs = ["./data_for_unit_tests/train.csv", "./data_for_unit_tests/validation.csv", "./data_for_unit_tests/test.csv"]
-		self.assertEqual(len(organize_files(csvs)), 3)
+	def test_df_to_csv_file_accuracy(self):
+		df = pd.DataFrame({'img_path':['a.jpg','b.jpg','c.jpg'],'truth':[0,1,1]})
+		csv_filename = 'temp_unit_testing.csv'
+		overwrite = False
+		df_to_csv(df, csv_filename, overwrite)
+		df_upload = pd.read_csv(csv_filename)
+		self.assertTrue(df.equals(df_upload))
+
 
 if __name__ == '__main__':
     unittest.main()
